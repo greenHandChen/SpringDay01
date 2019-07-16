e0a1ea08c0e54d37700946f826ce65f617ee53f7
YangHongKun <1059532208@qq.com>
2019-07-16 20:51:44 +0800

    25580_杨鸿坤

--- SpringDemo1/src/Client/Client.java
+++ SpringDemo1/src/Client/Client.java
@@ -1,7 +1,7 @@
 package Client;
 
 import Service.IAnnotationService;
-import SpringTest.test;
+import SpringTest.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,7 +15,26 @@ public class Client {
         System.out.println(t.getName());
         System.out.println(t.getId());
         System.out.println("--------------");
+
         IAnnotationService as=(IAnnotationService) ac.getBean("annotationServiceImpl");
         as.addUser();
+        System.out.println("--------------");
+
+        HelloSpring hs=(HelloSpring) ac.getBean("helloSpring");
+        hs.initBean();
+        hs.destoryBean();
+        System.out.println("--------------");
+
+        MyBeanPostProcessor mb=(MyBeanPostProcessor) ac.getBean("myBeanPostProcessor");
+        mb.postProcessAfterInitialization(t,"1");
+        mb.postProcessBeforeInitialization(t,"2");
+        System.out.println("--------------");
+
+        Person p=(Person) ac.getBean("person");
+        System.out.println(p.toString());
+        System.out.println("--------------");
+
+        CollectionBean cb=ac.getBean("collectionBean",CollectionBean.class);
+        System.out.println(cb.toString());
     }
 }

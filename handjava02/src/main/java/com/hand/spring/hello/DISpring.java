14162d000322db0ba0fc0289ebd235c5899101c8
Zero-LWY <1243426954@qq.com>
2019-07-16 19:26:13 +0800

    25593_练伟渊

--- handjava02/src/main/java/com/hand/spring/hello/DISpring.java
+++ handjava02/src/main/java/com/hand/spring/hello/DISpring.java
@@ -1,5 +1,7 @@
 package com.hand.spring.hello;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author lwy
  * @version V1.0
@@ -7,6 +9,7 @@ package com.hand.spring.hello;
  * @Description: TODO
  * @date 2019/7/16 16:22
  */
+@Component
 public class DISpring {
     private String DIString;
 

fd1d9a55cca04d9cee812395b8d469f91d4a32bc
zhangqiang77773 <572649208@qq.com>
2019-07-16 22:00:47 +0800

    上传DIString

--- 25208_张强/springIocTest/src/main/java/pojo/DISpring.java
+++ 25208_张强/springIocTest/src/main/java/pojo/DISpring.java
@@ -4,5 +4,13 @@ package pojo;
  * Created by zq on 2019/7/16
  */
 public class DISpring {
+    private String info;
 
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }

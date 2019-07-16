9e5e2f231837216683a9770408734596a588a8fc
YangHongKun <1059532208@qq.com>
2019-07-16 18:56:25 +0800

    25580_杨鸿坤

--- src/Repository/AnnotationRepository.java
+++
@@ -1,5 +0,0 @@
-package Repository;
-
-public interface AnnotationRepository {
-    void addUser();
-}
--- src/Service/IAnnotationService.java
+++
@@ -1,5 +0,0 @@
-package Service;
-
-public interface IAnnotationService {
-    void addUser();
-}

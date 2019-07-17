9d7e30831e2e58d153568d337d2dcdded21ebf6c
lq <1414709456@qq.com>
2019-07-17 21:23:51 +0800

    day02

--- parent/day2child/src/main/java/com/hand/lq/service/AccountServiceImpl.java
+++ parent/day2child/src/main/java/com/hand/lq/service/AccountServiceImpl.java
@@ -7,32 +7,39 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.ArrayList;
+
 @Service
 public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountDao accountDao;
     @Autowired
     private TransactionTemplate transactionTemplate;
+
     @Override
     public void transfer(String outUser, String inUser, Double money) {
-        transactionTemplate.execute(TransactionStatus->{
-                    accountDao.out(outUser,money);
-                    int number=1/0;
-                    accountDao.in(inUser,money);
-                    return  null;
+       transactionTemplate.execute(TransactionStatus -> {
+                    accountDao.out(outUser, money);
+                    int number = 1 / 0;
+                    accountDao.in(inUser, money);
+                    return null;
                 }
+
         );
 
+
     }
+
     public void transfer2(String outUser, String inUser, Double money) {
-        accountDao.out(outUser,money);
-        int number=1/0;
-        accountDao.in(inUser,money);
+        accountDao.out(outUser, money);
+        int number = 1 / 0;
+        accountDao.in(inUser, money);
     }
+
     @Transactional(rollbackFor = Exception.class)
     public void transfer3(String outUser, String inUser, Double money) {
-        accountDao.out(outUser,money);
-        int number=1/0;
-        accountDao.in(inUser,money);
+        accountDao.out(outUser, money);
+        int number = 1 / 0;
+        accountDao.in(inUser, money);
     }
 }

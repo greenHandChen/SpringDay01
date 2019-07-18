f50b0c304991ff2f2cf422f2b8b446a42a52199b
dingwensheng <dingwensheng@qq.com>
2019-07-18 19:21:45 +0800

    传错了删除

--- SpringDay02/spring_transaction02/src/main/java/com/hand/service/AcountService.java
+++
@@ -1,5 +0,0 @@
-package com.hand.service;
-
-public interface AcountService {
-    public void transfer(String outer, String inner, double money);
-}
--- SpringDay02/spring_transaction03/src/main/java/com/hand/dao/AcountDao.java
+++
@@ -1,6 +0,0 @@
-package com.hand.dao;
-
-public interface AcountDao {
-    public void in(String inner, double money);
-    public void out(String outer, double money);
-}

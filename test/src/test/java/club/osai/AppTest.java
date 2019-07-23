1db2f78bd7cd0138076cfdbbcfed6150577a9bd8
ChenQuan <chenquan@osai.club>
2019-07-23 20:46:09 +0800

    delete file

---
+++ test/src/test/java/club/osai/AppTest.java
@@ -0,0 +1,20 @@
+package club.osai;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

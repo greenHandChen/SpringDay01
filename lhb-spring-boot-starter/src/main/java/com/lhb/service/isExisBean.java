468813ba3446dc14daa2089c5a8cd19a823a3707
Alex531003231 <531003231@qq.com>
2019-07-23 21:32:52 +0800

    添加判断

--- lhb-spring-boot-starter/src/main/java/com/lhb/service/isExisBean.java
+++ lhb-spring-boot-starter/src/main/java/com/lhb/service/isExisBean.java
@@ -8,7 +8,12 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 /**
  * Created By LHB on 2019/7/23;
  */
+
+/**
+ * 判断有没有Bean
+ */
 public class isExisBean implements Condition {
+
     public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
         ConfigurableListableBeanFactory beanFactory = conditionContext.getBeanFactory();
 

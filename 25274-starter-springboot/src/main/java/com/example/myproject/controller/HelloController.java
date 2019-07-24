c4d7aa0e88b9baed5c4873c4841239d4607be26f
MofeiTang <1625399001@qq.com>
2019-07-24 10:39:54 +0800

    修改版

---
+++ 25274-starter-springboot/src/main/java/com/example/myproject/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.myproject.controller;
+
+import com.txz.HelloSpringBoot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloSpringBoot helloSpringBoot;
+
+    @GetMapping("/helloSpringBoot")
+    public String helloSpring(){
+        return helloSpringBoot.sayHello();
+    }
+
+}

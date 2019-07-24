f522d4b52766d40f728c67196ce1dc20b21978ae
yuehua_lan <813756517@qq.com>
2019-07-24 08:47:44 +0800

    7_23小作业

--- springboot25903/src/main/java/com/hand/springboot25903/controller/testController.java
+++ springboot25903/src/main/java/com/hand/springboot25903/controller/testController.java
@@ -1,15 +1,21 @@
-package com.hand.controller;
+package com.hand.springboot25903.controller;
 
+import com.hand.HelloSpringBoot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class testController {
+
      @Autowired
      private HelloSpringBoot helloSpringBoot;
+
      @GetMapping("/helloSpringBoot")
-     public void hellobootspring(){
-          helloSpringBoot.sayHello;
+     @ResponseBody
+     public String hellobootspring(){
+          helloSpringBoot.sayHello();
+          return "成功";
      }
 }
